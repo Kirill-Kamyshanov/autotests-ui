618f3113b8e1ac3525b4ed3bc03b5428d11bc92e
Kirill-Kamyshanov <nestor10000@mail.ru>
2026-06-14 10:32:57 +0300

    Знакомство с PageComponent

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -19,14 +19,18 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 
 
 @pytest.fixture
-def dashboard_page(chromium_page: Page) -> DashboardPage:
-    return DashboardPage(page=chromium_page)
-
-@pytest.fixture
 def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
-
 
 @pytest.fixture
 def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
     return CreateCoursePage(page=chromium_page_with_state)
+
+
+@pytest.fixture
+def dashboard_page(chromium_page: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page)
+
+@pytest.fixture
+def dashboard_page_with_state(chromium_page_with_state: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page_with_state)
