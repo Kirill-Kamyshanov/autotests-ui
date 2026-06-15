867496419631d21806a703cc098c6b75513b3d33
Kirill-Kamyshanov <nestor10000@mail.ru>
2026-06-15 14:54:16 +0300

    Наращивание тестовой базы и рефакторинг

--- tests/authentification/test_registration.py
+++ tests/authentification/test_registration.py
@@ -6,9 +6,10 @@ from pages.authentification.registration_page import RegistrationPage
 
 @pytest.mark.registration
 @pytest.mark.regression
-def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
-    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.registration_form.fill(email="user@gmail.com", password="password", username="username")
-    registration_page.registration_form.check_visible(email="user@gmail.com", password="password", username="username")
-    registration_page.click_registration_button()
-    dashboard_page.dashboard_toolbar_view.check_visible()
+class TestRegistration:
+    def test_successful_registration(self, registration_page: RegistrationPage, dashboard_page: DashboardPage):
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.registration_form.fill(email="user@gmail.com", password="password", username="username")
+        registration_page.registration_form.check_visible(email="user@gmail.com", password="password", username="username")
+        registration_page.click_registration_button()
+        dashboard_page.dashboard_toolbar_view.check_visible()
