1b814eb4e6a13619d78a3909253c5a1552db8903
Kirill-Kamyshanov <nestor10000@mail.ru>
2026-06-23 20:32:00 +0300

    Итоговый проект

--- tools/logger.py
+++ tools/logger.py
@@ -3,10 +3,10 @@ import logging
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.WARNING)
 
     handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
+    handler.setLevel(logging.WARNING)
 
     formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
     handler.setFormatter(formatter)
